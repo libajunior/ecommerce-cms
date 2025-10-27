c8df3059a7f1870a1f5767dae352ae3ac4250611
Libório Júnior <libajunior@gmail.com>
2025-10-27 15:00:23 -0300

    Aula 22/10/2025 - Implementação dos componentes do case Categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { CategoryLayout } from "./cases/categories/components/category-layout"
 
 function App() {
 
   return (
     <div className="wrapper">
-      
+      <CategoryLayout />
     </div>
   )
 }
